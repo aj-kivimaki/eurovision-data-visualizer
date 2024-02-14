49a7217453362a50fb50f6ba50474e2f1bc41548
JerryU <jerry.uusitalo@gmail.com>
2024-02-14 18:20:00 +0200

    Add initial styling for Navbar component

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,5 +1,24 @@
+import { AppBar, Box, Typography, TextField, Toolbar } from "@mui/material";
+
 const NavBar: React.FC = () => {
-  return <div>NavBar</div>;
+  return (
+    <Box>
+      <AppBar
+        position="static"
+        sx={{ backgroundColor: "#FF43EC", color: "#FFFFFF" }}
+      >
+        <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
+          <Typography noWrap={true} sx={{ flexBasis: "33%" }}>
+            Eurovision Data Visualizer
+          </Typography>
+          <TextField label="Search" variant="standard" sx={{}} />
+          <Typography sx={{ flexBasis: "33%", textAlign: "right" }}>
+            STATISTICS
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 };
 
 export default NavBar;
