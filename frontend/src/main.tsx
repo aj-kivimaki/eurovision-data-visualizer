2d31ea733bdcadfc64be427d4d8ce0ae74766c32
JerryU <jerry.uusitalo@gmail.com>
2024-02-14 17:08:45 +0200

    Initialize themeProvider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { ThemeProvider } from "@emotion/react";
+import { createTheme } from "@mui/material";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#85F668",
+    },
+    secondary: {
+      main: "#46BCFF"
+    }
+  }
+})
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
