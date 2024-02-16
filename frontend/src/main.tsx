8039da0207dbe6fe5adaa243e333286428bf4126
JerryU <jerry.uusitalo@gmail.com>
2024-02-16 13:42:19 +0200

    Create styles for TextField in the themeProvider, import Theme type

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,17 +4,31 @@ import App from "./App.tsx";
 import "./index.css";
 import { ThemeProvider } from "@emotion/react";
 import { createTheme } from "@mui/material";
+import { Theme } from "@mui/material";
 
-const theme = createTheme({
-  palette: { // palette documentation https://mui.com/material-ui/customization/palette/
+const theme : Theme = createTheme({
+  palette: {
+    // palette documentation https://mui.com/material-ui/customization/palette/
     primary: {
       main: "#85F668",
     },
     secondary: {
-      main: "#46BCFF"
-    }
-  }
-})
+      main: "#46BCFF",
+    },
+  },
+  components: {
+    MuiTextField: {
+      styleOverrides: { // https://mui.com/material-ui/customization/theme-components/#theme-style-overrides
+        root: {
+          "& input": { color: "#FFFFFF" },
+          "& .MuiOutlinedInput-notchedOutline": { // Set default border color to white
+            borderColor: "#FFFFFF",
+          }, 
+        },
+      },
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
