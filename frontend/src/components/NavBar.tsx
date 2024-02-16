6b1e97b2ab9acf7b538003559dd51ad93cdafcd8
JerryU <jerry.uusitalo@gmail.com>
2024-02-16 10:05:51 +0200

    Add custom styles for input element

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,17 +1,53 @@
-import { AppBar, Box, Typography, TextField, Toolbar } from "@mui/material";
+import { useState } from "react";
+import {
+  AppBar,
+  Box,
+  Typography,
+  TextField,
+  Toolbar,
+  InputAdornment
+} from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
 
 const NavBar: React.FC = () => {
+  const [isFocused, setIsFocused] = useState<boolean>(false);
   return (
     <Box>
       <AppBar
         position="static"
         sx={{ backgroundColor: "#FF43EC", color: "#FFFFFF" }}
       >
-        <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
+        <Toolbar
+          sx={{ display: "flex", justifyContent: "space-between", my: 1 }}
+        >
           <Typography noWrap={true} sx={{ flexBasis: "33%" }}>
             Eurovision Data Visualizer
           </Typography>
-          <TextField label="Search" variant="standard" sx={{}} />
+          <TextField
+            label="Search"
+            variant="outlined"
+            sx={{
+              "& input": { color: "#FFFFFF" },
+              "& input::hover": { borderColor: "#FFFFFF" },
+              "& input::placeholder": { color: "#FFFFFF" },
+              "& input:focus": { borderColor: "#FFFFFF" },
+              "& .MuiOutlinedInput-notchedOutline": { borderColor: "#FFFFFF" },
+              '&:hover .MuiOutlinedInput-notchedOutline': { borderColor: "primary.main" },
+            }}
+            focused={isFocused}
+            onFocus={() => setIsFocused(true)}
+            onBlur={() => setIsFocused(false)}
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <SearchIcon
+                    sx={{ color: isFocused ? "primary.main" : "#FFFFFF" }}
+                  />
+                </InputAdornment>
+              ),
+            }}
+            InputLabelProps={{sx : { color: "#FFFFFF"}}}
+          />
           <Typography sx={{ flexBasis: "33%", textAlign: "right" }}>
             STATISTICS
           </Typography>
