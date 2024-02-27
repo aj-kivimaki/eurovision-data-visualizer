b64e4fd1ccae5284d5e8f69be346fd2bebcfbb6c
JerryU <jerry.uusitalo@gmail.com>
2024-02-27 15:36:25 +0200

    Create initial styling for cards container and cards introduction

--- frontend/src/routes/Home.tsx
+++ frontend/src/routes/Home.tsx
@@ -1,11 +1,27 @@
 import Banner from "../components/Banner";
+import { Box, Typography } from "@mui/material";
 
 const Home: React.FC = () => {
   return (
-    <div>
+    <Box>
       <Banner />
-      <div>Home</div>
-    </div>
+        <Box sx={{ backgroundColor: "primary.main", height: "500px", p: 2}}>
+          <Typography variant="h1"
+            sx={{
+              fontSize: {
+                xs: "2rem",
+                sm: "3rem",
+              },
+              textAlign: "center"
+            }}
+          >
+            Welcome to Eurovision data visualizer app!
+          </Typography>
+        </Box>
+        <Box sx={{backgroundColor: "#1c1c1c", color:"#FFFFFF", height: "500px"}}>
+            Cards will appear here
+        </Box>
+    </Box>
   );
 };
 
