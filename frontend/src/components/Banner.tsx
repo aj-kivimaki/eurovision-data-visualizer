91d0b9bd10e5308bd7fa66bad4ec2ebff2b58a50
JerryU <jerry.uusitalo@gmail.com>
2024-02-27 14:40:54 +0200

    Create Banner component with initial styling

--- frontend/src/components/Banner.tsx
+++ frontend/src/components/Banner.tsx
@@ -1,5 +1,21 @@
+import { Box } from "@mui/material";
+
 const Banner: React.FC = () => {
-  return <div>Banner</div>;
+  return (
+    <Box
+      sx={{
+        backgroundImage:
+          "url(https://images.unsplash.com/photo-1542732351-fa2c82b0c746?q=80&w=3432&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)",
+        width: "100%",
+        height: "500px",
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center bottom"
+      }}
+    >
+      Banner
+    </Box>
+  );
 };
 
 export default Banner;
